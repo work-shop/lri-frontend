a2fce6e83cca081214909d9c918a09e2690044c7
Greg Nemes <mail@gregnemes.com>
2017-02-22 21:56:55 -0500

    significant progress on home page

--- static/javascript/menu-utilities.js
+++ static/javascript/menu-utilities.js
@@ -2,6 +2,28 @@
 
 module.exports = function($) {
 
+	//set up the menu and events that interact with it 
+	function setupMenus() {
+
+		$( document ).ready( function() {
+			$('.menu-toggle').click(function(e) {
+				e.preventDefault();
+				menuToggle();
+			});			
+		});
+
+
+		$( document ).ready( function() {
+			$('.menu-field-toggle').click(function(e) {
+				var menu = $(this).data('menu');
+				e.preventDefault();
+				menuFieldToggle( menu, $(this) );
+			});			
+		});		
+
+	}
+
+
 	//open and close the menu
 	function menuToggle(){
 
@@ -19,10 +41,11 @@ module.exports = function($) {
 	}
 
 	//open and close the menu fields
-	function menuFieldToggle( menu ){
+	function menuFieldToggle( menu, link ){
 
 		menu = '#menu-' + menu;
 		var $menu = $(menu);
+		var $link = link;
 
 		if( $menu.hasClass('open') && $('body').hasClass('menu-open') ){
 			console.log('the targeted menu is open, so close it');
@@ -30,49 +53,31 @@ module.exports = function($) {
 		} else if ( $menu.hasClass('closed') && $('body').hasClass('menu-open') ){
 			console.log('the targeted menu is closed, but another menu is open. close the other menu(s), and open the target');
 			$('.menu-field').removeClass('open').addClass('closed');
-			openFieldMenu( $menu );			
+			$('.menu-field-link').removeClass('active').addClass('inactive');			
+			openFieldMenu( $menu, $link );			
 		} else if ( $menu.hasClass('closed') && $('body').hasClass('menu-closed') ){
 			console.log('the targeted menu is closed, and no other menus are open. Open the target');			
-			openFieldMenu( $menu );	
+			openFieldMenu( $menu, $link );	
 		}
 
 	}	
 
 	//close a specific field menu
 	function closeFieldMenu( $menu ){		
+		$('.menu-field-link').removeClass('active').addClass('inactive');
 		$menu.removeClass('open').addClass('closed');
 		$('#blanket').removeClass('on').addClass('off');			
 		$('body').removeClass('menu-open').addClass('menu-closed');
 	}
 
-	//close a specific field menu
-	function openFieldMenu( $menu ){		
+	//open a specific field menu
+	function openFieldMenu( $menu, $link ){		
+		$link.removeClass('inactive').addClass('active');
 		$menu.removeClass('closed').addClass('open');
 		$('#blanket').removeClass('off').addClass('on');			
 		$('body').removeClass('menu-closed').addClass('menu-open');
 	}
 
-
-	//set up the menu and events that interact with it 
-	function setupMenus() {
-
-		$( document ).ready( function() {
-			$('.menu-toggle').click(function(e) {
-				e.preventDefault();
-				menuToggle();
-			});			
-		});
-
-
-		$( document ).ready( function() {
-			$('.menu-field-toggle').click(function(e) {
-				var menu = $(this).data('menu');
-				e.preventDefault();
-				menuFieldToggle( menu );
-			});			
-		});		
-
-	}
 
 	return {
 		menuToggle: menuToggle,
