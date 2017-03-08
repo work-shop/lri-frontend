f890aa8a8011a6cd39a1a2022493770e63197144
Greg Nemes <mail@gregnemes.com>
2017-03-08 08:36:18 -0500

    built out alumni directory filtering to 98%

--- static/javascript/directory.js
+++ static/javascript/directory.js
@@ -3,9 +3,9 @@
 
 module.exports = function($, configuration) {
 
-
 	var base = configuration.directory_endpoint;
-	var $directory_container = $('#directory_container');
+	var $directoryContainer = $('#directory_container');
+	var $dCount = $('#d-count');
 	var m1 = '<tr class="d-item"><td class="d-name">';
 	var m3 = '</td><td class="d-year">';
 	var m5 = '</td><td class="d-organization">';
@@ -16,13 +16,16 @@ module.exports = function($, configuration) {
 
 
 	function getAlumni( classType, classYear ){
+
+		emptyDirectory();
 		
 		if( typeof classType === 'undefined' ){
-			classType = initialType;
-			classYear = initialYear;
+			var _classType = initialType;
+			var _classYear = initialYear;
+			requestAlumni( _classType, _classYear );	
+		} else{
+			requestAlumni( classType, classYear );		
 		}
-
-		requestAlumni( 'lri', '2016' );		
 
 	}
 
@@ -53,45 +56,91 @@ module.exports = function($, configuration) {
 	function parse( response ){
 
 		var alumni = response.content.records;
+		renderMeta( alumni );
 
 		if( typeof alumni !== 'undefined'){
 			if ( alumni.length > 0 ){
 				for (var i = 0; i < alumni.length; i++) {
-					render( alumni[i] );
+					renderItem( alumni[i] );
 				}
 			}
 		}
 	}
 
 
-	function render( item ){
+	function renderItem( item ){
 		
 		var m2,m4,m6,m8;
 		
-		m2 = item.Name;
-		m4 = item.Class_Year__c;
-		m6 = item.Current_Employer__c;	
-		m8 = item.Current_Title__c;
+		if ( item.Name === null){
+			m2 = '';
+		} else{
+			m2 = item.Name;
+		}
+		if ( item.Class_Year__c === null){
+			m4 = '';
+		} else{
+			m4 = item.Class_Year__c;
+		}
+		if ( item.Current_Employer__c === null){
+			m6 = '';
+		} else{
+			m6 = item.Current_Employer__c;
+		}
+		if ( item.Current_Title__c === null){
+			m8 = '';
+		} else{
+			m8 = item.Current_Title__c;
+		}
 
 		var markup = m1+m2+m3+m4+m5+m6+m7+m8+m9;
 
-		$directory_container.append( markup );
+		$directoryContainer.append( markup );
 
 	}	
+
+
+	function renderMeta( items ){
+		
+		var count = items.length;
+
+		$dCount.text( count );
+		
+
+
+	}	
+
+
+	//remove all items from the directory
+	function emptyDirectory(){
+		$('.d-item').remove();
+	}
+
+
+	function setupAlumni(){
+		$( ".d-select" ).change(function() {
+			var ct = $('#d-class-type').val();
+			var cy = $('#d-class-year').val();
+			getAlumni( ct, cy );
+		});
+
+	}
 
 
 	//setup this process
 	function initialize() {
 
 		$( document ).ready( function() {
-			window.requestAnimationFrame( getAlumni );	
+			getAlumni( initialType, initialYear);	
+			setupAlumni();	
 		});
 
 	}
 
 	//return an object with methods that correspond to above defined functions
 	return {
-		initialize: initialize
+		initialize: initialize,
+		getAlumni: getAlumni,
 	};
 
 };
